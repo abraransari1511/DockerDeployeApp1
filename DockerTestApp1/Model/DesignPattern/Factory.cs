640c37ec36738c1830b98dc4502f20134ed6fc45
Abrar Ansari <abrar.ansari1511@gmail.com>
2020-05-16 22:36:12 +0300

    adding some files

---
+++ DockerTestApp1/Model/DesignPattern/Factory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerTestApp1.Model.DesignPattern
+{
+    interface customer
+    {
+
+    }
+
+    class customerclassA : customer
+    {
+    }
+
+    class customerclassB : customer
+    {
+    }
+
+    abstract class classA
+    {
+        public abstract customer FactoryMethod(string type);
+    }
+
+    class classB : classA
+    {
+        public override customer FactoryMethod(string type)
+        {
+            switch (type)
+            {
+                case "A": return new customerclassA();
+                case "B": return new customerclassB();
+                default: throw new ArgumentException("Invalid type", "type");
+            }
+        }
+    }
+}
---
+++ DockerTestApp1/Model/DesignPattern/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace DockerTestApp1.Model.DesignPattern
+{
+    public class Singleton
+    {
+        private Singleton()
+        {
+
+        }
+        public volatile static Singleton singletonObject = null;
+        public static Singleton GetSingletonInstance()
+        {
+            object lockingObject = new object();
+            if (singletonObject==null)
+            {
+                lock (lockingObject)
+                {
+                    if (singletonObject == null)
+                    {
+                        singletonObject = new Singleton();
+                    }
+                }
+            }
+            return singletonObject;
+        }
+
+    }
+}
